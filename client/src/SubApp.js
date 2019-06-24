f51f550a0c900155b196d11cb69e06f8c2879cbd
Atul Shukla <atul@phonexinc.com>
2019-06-24 17:56:56 -0400

    multi docker working v1!

--- client/src/SubApp.js
+++ client/src/SubApp.js
@@ -1,28 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import SubApp from './SubApp';
 
-export default function App() {
+export default function SubApp() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     axios
-      .get("http://localhost:5001/api/values")
+      .get("http://localhost:5002/api/ethics")
       .then(result => setData(result.data));
   }, []);
 
   return (
     <div>
-      <p>Hello Values!</p>
+      <p>Hello Ethics!</p>
       <ul>
         {data.map(item => (
-          <li key={item.name}>
-            {item.name}: {item.value}
+          <li key={item}>
+            {item}
           </li>
         ))}
       </ul>
       <hr />
-      <SubApp />
     </div>
   );
 }
